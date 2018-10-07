80a7622d616a6f5d12c65283f4d0ddb2137a1be2
Mateusz Kiełbasa <sobriodev@gmail.com>
2018-10-07 20:05:48 +0200

    Functions rebuild, optimization, bug fixes, new enums, macros and comments

--- inc/max7219.h
+++ inc/max7219.h
@@ -15,29 +15,25 @@
  * @{
  */
 
-#define MAX7219_NO_OP_REG           0x00            /**< The No-Op register used for sending dummy data */
-#define MAX7219_DECODE_MODE_REG     0x09            /**< Decode Mode register used for turning on/off Code B decoding */
-#define MAX7219_INTENSITY_REG       0x0A            /**< Intensity register used for setting LEDs' brightness */
-#define MAX7219_SCAN_LIMIT_REG      0x0B            /**< Scan Limit register used for setting maximum number of scanned digits */
-#define MAX7219_SHOUTDOWN_REG       0x0C            /**< Shutdown register used for turning on/off the display */
-#define MAX7219_DISPLAY_TEST_REG    0x0F            /**< Display Test register used for turning on/off test mode (all LEDs) */
-#define MAX7219_DIGIT_REG(DIGIT)    ((DIGIT) + 1)   /**< Calculate proper digit register from 0 offset.
-                                                         The Digit registers starts from 0x01 (1st digit) to 0x08 (8th digit) */
-/**
- *  Macro for building MAX7219 frames. The frame format is:
- *  |DON'T CARE BITS (B12:B15)|ADDR (B8:B11)|DATA (B0:B7)|
- */
-#define MAX7219_FRAME(ADDR, DATA)    ((uint16_t) (((ADDR) << 8) | ((DATA) & 0xFF)))
+#define MAX7219_NO_OP_REG           0x00    //!< No-Op register used for sending dummy data
+#define MAX7219_DECODE_MODE_REG     0x09    //!< Decode Mode register used for turning on/off Code B decoding
+#define MAX7219_INTENSITY_REG       0x0A    //!< Intensity register used for setting LEDs' brightness
+#define MAX7219_SCAN_LIMIT_REG      0x0B    //!< Scan Limit register used for setting maximum number of scanned digits
+#define MAX7219_SHOUTDOWN_REG       0x0C    //!< Shutdown register used for turning on/off the display
+#define MAX7219_DISPLAY_TEST_REG    0x0F    //!< Display Test register used for turning on/off test mode (all LEDs)
 
-#define LOW     0   /**<  Low state of a pin indicates presence of data */
-#define HIGH    1   /**<  High state of a pin latches data in all MAX7219 chips */
+/**< Calculate proper digit register from 0 offset. The Digit registers start from 0x01 (1st digit) and end at 0x08 (8th digit) */
+#define MAX7219_DIG_REG(DIG)    ((DIG) + 1)
 
-#define MAX_DISP_OFFSET(DISP)       ((DISP) - 1)        /**< Calculate maximum display offset */
-#define MAX_COL_OFFSET(COL)         ((COL) * 8 - 1)     /**< Calculate maximum column offset */
-#define NEW_COL_OFFSET(COL, LEN)    ((COL) + (LEN) - 1) /**< Calculate new column offset based on current column and length of the data */
+#define COLS_PER_DISP 8 //!< The number of columns per display
 
-#define REAL_DISP(COL)  ((COL) / 8) /**< Calculate output display based on column offset */
-#define REAL_COL(COL)   ((COL) % 8) /**< Calculate real column based on column offset */
+#define LOW     0   //!<  Low state of a pin indicates presence of data
+#define HIGH    1   //!<  High state of a pin latches data in all MAX7219 chips
+
+#define MAX_DISP_OFFSET(DISP)       ((DISP) - 1)            //!< Calculate maximum display offset
+#define MAX_COL_OFFSET(COL)         ((COL) * 8 - 1)         //!< Calculate maximum column offset
+#define END_COL_OFFSET(COL, LEN)    ((COL) + (LEN) - 1)     //!< Calculate end column offset based on current column and length of the data
+#define COL_OFFSET(DISP, COL)       ((DISP) * 8 + (COL))    //!< Calculate absolute column offset based on a specified display column
 
 /**
  * @brief MAX7219 configuration structure
@@ -47,19 +43,40 @@
  * @note SSEL pin have to be configured as a GPIO output pin. See GPIO byte pin registers for passing base address
  */
 typedef struct {
-    LPC_SSP_T *ssp;         /**< The base of SSP used */
-    __IO uint8_t *ssel;     /**< The base of GPIO pin used */
-    size_t numOfDisplays;   /**< The number of chained MAX7219s (number of displays). */
+    LPC_SSP_T *ssp;     //!< The base of SSP used
+    __IO uint8_t *ssel; //!< The base of GPIO pin used
+    uint8_t numOfDisp;  //!< The number of chained MAX7219s (number of displays)
 } MAX7219_Config;
 
 /**
  * @brief Return codes for library functions
  */
 typedef enum {
-    OP_SUCCESS = 0,     /**< Operation performed successfully */
-    BUFFER_ERROR = 1,   /**< Buffer allocation error */
-    OFFSET_ERROR = 2,   /**< Given display/data offset exceeds correct range */
+    OP_SUCCESS = 0,     //!< Operation performed successfully
+    BUFFER_ERROR = 1,   //!< Buffer allocation error
+    OFFSET_ERROR = 2    //!< Given display/data offset exceeds correct range
 } MAX7219_ReturnCodes;
+
+/**
+ * @brief Modes for updating display buffer
+ */
+typedef enum {
+    VAL_UPDATE = 0, //!< Update multiple buffer bytes using single byte
+    SEQ_UPDATE = 1  //!< Update the buffer with a sequence of bytes
+} MAX7219_BufferUpdateModes;
+
+/**
+ * Build MAX7219 frame
+ * @param addr : Register address
+ * @param data : Specified data
+ * @return Built frame
+ * @note
+ * The MAX7219 frame format is:
+ * [ DON'T CARE BITS (B12:B15) | ADDR (B8:B11) | DATA (B0:B7) ]
+ */
+static inline uint16_t MAX7219_Frame(uint8_t addr, uint8_t data) {
+    return ((uint16_t) (((addr) << 8) | ((data) & 0xFF)));
+}
 
 /**
  * @brief Helper function for calculating intensity frame
@@ -67,8 +84,8 @@ typedef enum {
  * @return Calculated frame
  * @note Only lower nibble of the value is recognized. Values from 0x0 (minimum intensity) to 0xF (maximum intensity) are acceptable
  */
-static inline uint16_t MAX7219_IntensityFrame(uint8_t intensity) {
-    return MAX7219_FRAME(MAX7219_INTENSITY_REG, intensity);
+static inline uint16_t MAX7219_Intensity(uint8_t intensity) {
+    return MAX7219_Frame(MAX7219_INTENSITY_REG, intensity);
 }
 
 /**
@@ -76,8 +93,8 @@ static inline uint16_t MAX7219_IntensityFrame(uint8_t intensity) {
  * @param mode : Shutdown mode. Passing true turns off the display, false turns it on
  * @return Calculated frame
  */
-static inline uint16_t MAX7219_ShutdownFrame(bool mode) {
-    return MAX7219_FRAME(MAX7219_SHOUTDOWN_REG, !mode);
+static inline uint16_t MAX7219_Shutdown(bool mode) {
+    return MAX7219_Frame(MAX7219_SHOUTDOWN_REG, !mode);
 }
 
 /**
@@ -85,20 +102,20 @@ static inline uint16_t MAX7219_ShutdownFrame(bool mode) {
  * @param mode : Test mode. Passing true turns on test mode, false turns it off
  * @return Calculated frame
  */
-static inline uint16_t MAX7219_TestFrame(bool mode) {
-    return MAX7219_FRAME(MAX7219_DISPLAY_TEST_REG, mode);
+static inline uint16_t MAX7219_Test(bool mode) {
+    return MAX7219_Frame(MAX7219_DISPLAY_TEST_REG, mode);
 }
 
 /**
- * @brief Set all necessary settings, configure MAX7219s' blocks and allocate buffer data
+ * @brief Set all necessary settings, configure MAX7219 blocks and allocate buffer data
  * @param config : An MAX7219_Config entity.
  * @return The function can return:
  *           - BUFFER_ERROR when data buffer cannot be allocated
  *           - OP_SUCCESS when everything is set correctly
- * @note Only positive values for MAX7219_Config.numOfDisplays are allowed. Passing 0 will cause change the variable value to 1
+ * @note Only positive values for MAX7219_Config.numOfDisp are allowed. Passing 0 will cause change the variable value to 1
  * @note Always check the return code. Behavior of library functions is undefined if this function does not return OP_SUCCESS code
  */
-MAX7219_ReturnCodes MAX7219_SetConfiguration(MAX7219_Config config);
+MAX7219_ReturnCodes MAX7219_SetConf(MAX7219_Config config);
 
 /**
  * @brief Update the registry value of the desired display
@@ -108,7 +125,7 @@ MAX7219_ReturnCodes MAX7219_SetConfiguration(MAX7219_Config config);
  *           - OFFSET_ERROR when given offset exceeds total numbers of displays
  *           - OP_SUCCESS otherwise
  */
-MAX7219_ReturnCodes MAX7219_UpdateDisplayReg(size_t dispOffset, uint16_t frame);
+MAX7219_ReturnCodes MAX7219_UpdateDisplayReg(uint8_t dispOffset, uint16_t frame);
 
 /**
  * @brief Update the registry value of all displays
@@ -117,19 +134,61 @@ MAX7219_ReturnCodes MAX7219_UpdateDisplayReg(size_t dispOffset, uint16_t frame);
  */
 void MAX7219_UpdateDisplaysReg(uint16_t frame);
 
-MAX7219_ReturnCodes MAX7219_UpdateBuffer(size_t colOffset, const uint8_t *data, size_t bytes, bool opCode);
+/**
+ * @brief Write bytes to the output buffer
+ * @param colOffset : Column offset starting from 0
+ * @param data      : Data base address
+ * @param bytes     : The Number of bytes written to the buffer
+ * @param opCode    : Operation code. Supported values:
+ *                     - VAL_UPDATE : Update N bytes of the buffer with the same value. Only first byte of the data is recognized
+ *                     - SEQ_UPDATE : Fill the buffer with N bytes of data
+ * @return Multiple return values are supported:
+ *          - OFFSET_ERROR when data will not fit into the buffer
+ *          - OP_SUCCESS otherwise
+ * @see MAX7219_UpdateBufferVal() and MAX7219_BufferSeq() functions for more convenient updating of the buffer
+ */
+MAX7219_ReturnCodes MAX7219_UpdateBuffer(uint16_t colOffset, const uint8_t *data, uint16_t bytes, MAX7219_BufferUpdateModes mode);
 
-static inline MAX7219_ReturnCodes MAX7219_UpdateBufferPtr(size_t colOffset, const uint8_t *data, size_t bytes) {
-    return MAX7219_UpdateBuffer(colOffset, data, bytes, false);
+/**
+ * @brief Write data to the output buffer
+ * @param colOffset : Column offset starting from 0
+ * @param value     : Data base address
+ * @param bytes     : The number of bytes written to the buffer
+ * @return Multiple return values are supported:
+ *          - OFFSET_ERROR when data will not fit into the buffer
+ *          - OP_SUCCESS otherwise
+ * @note This is wrapper for the function MAX7219_UpdateBuffer()
+ */
+static inline MAX7219_ReturnCodes MAX7219_UpdateBufferSeq(uint16_t colOffset, const uint8_t *data, uint16_t bytes) {
+    return MAX7219_UpdateBuffer(colOffset, data, bytes, SEQ_UPDATE);
 }
 
-static inline MAX7219_ReturnCodes MAX7219_UpdateBufferVal(size_t colOffset, uint8_t value, size_t bytes) {
-    return MAX7219_UpdateBuffer(colOffset, &value, bytes, true);
+/**
+ * @brief Update output buffer columns with the same value
+ * @param colOffset : Column offset starting from 0
+ * @param data      : Data base address
+ * @param bytes     : The number of bytes written to the buffer
+ * @return Multiple return values are supported:
+ *          - OFFSET_ERROR when data will not fit into the buffer
+ *          - OP_SUCCESS otherwise
+ * @note This is wrapper for the function MAX7219_UpdateBuffer()
+ */
+static inline MAX7219_ReturnCodes MAX7219_UpdateBufferVal(uint16_t colOffset, uint8_t value, uint16_t bytes) {
+    return MAX7219_UpdateBuffer(colOffset, &value, bytes, VAL_UPDATE);
 }
 
+/**
+ * @brief Send data from the buffer to the displays
+ * @todo Modify the function to refresh only specified columns
+ * @return Nothing
+ */
 void MAX7219_RefreshDisp(void);
 
-void MAX7219_UnsetConfiguration(void);
+/**
+ * @brief Unset configuration and free allocated memory
+ * @return Nothing
+ */
+void MAX7219_UnsetConf(void);
 
 /**
  * @}
--- src/max7219.c
+++ src/max7219.c
@@ -13,8 +13,7 @@
  ****************************************************************************/
 
 static MAX7219_Config conf;     /**< Stores all necessary configuration parameters */
-static uint8_t *buffer = NULL;  /**< Buffer for storing displays data */
-
+static uint8_t *buffer = NULL;  /**< Buffer for storing output data */
 
 /*****************************************************************************
  * Public types/enumerations/variables
@@ -28,7 +27,7 @@ static uint8_t *buffer = NULL;  /**< Buffer for storing displays data */
  * @brief Wait until the TNF bit is 1 indicating that the TX FIFO is not full
  * @return Nothing
  */
-static inline void TNFWait(void) {
+static inline void SSP_TNFWait(void) {
     while (!(conf.ssp->SR & SSP_STAT_TNF));
 }
 
@@ -36,7 +35,7 @@ static inline void TNFWait(void) {
  * @brief Wait until the BSY bit is 0 indicating that the SSP controller is idle
  * @return Nothing
  */
-static inline void BSYWait(void) {
+static inline void SSP_BSYWait(void) {
     while (conf.ssp->SR & SSP_STAT_BSY);
 }
 
@@ -44,80 +43,77 @@ static inline void BSYWait(void) {
  * @brief Send 16-bit SPI frame
  * @param frame : Requested frame
  */
-static void sendSPIFrame(uint16_t frame) {
-    TNFWait(); /* Make sure that TX FIFO will handle the frame */
-    conf.ssp->DR = frame;
+static void SSP_SendFrame(uint16_t frame) {
+    SSP_TNFWait(); /* Make sure that TX FIFO will handle the frame */
+    Chip_SSP_SendFrame(conf.ssp, frame);
 }
 
 /*****************************************************************************
  * Public functions
  ****************************************************************************/
 
-MAX7219_ReturnCodes MAX7219_SetConfiguration(MAX7219_Config config) {
+MAX7219_ReturnCodes MAX7219_SetConf(MAX7219_Config config) {
     conf = config;
-    if (conf.numOfDisplays == 0) {
-        conf.numOfDisplays = 1; /* Change the number of displays to one if zero is passed */
+    if (conf.numOfDisp == 0) {
+        conf.numOfDisp = 1; /* Change the number of displays to one if zero is passed */
     }
 
-    buffer = (uint8_t *) malloc(sizeof(uint8_t) * conf.numOfDisplays);
+    buffer = (uint8_t *) malloc(sizeof(uint8_t) * conf.numOfDisp);
     if (buffer == NULL) {
         return BUFFER_ERROR; /* Memory cannot be allocated. Return error code */
     }
 
     *(conf.ssel) = HIGH; /* The SSEL pin have to be high when inactive */
-    MAX7219_UpdateDisplaysReg(MAX7219_FRAME(MAX7219_DECODE_MODE_REG, 0x00)); /* Disable Code B */
-    MAX7219_UpdateDisplaysReg(MAX7219_FRAME(MAX7219_SCAN_LIMIT_REG, 0x07)); /* Set scan limit to 8 digits */
-    MAX7219_UpdateDisplaysReg(MAX7219_FRAME(MAX7219_DISPLAY_TEST_REG, 0x00)); /* Disable test mode */
+    MAX7219_UpdateDisplaysReg(MAX7219_Frame(MAX7219_DECODE_MODE_REG, 0x00)); /* Disable Code B */
+    MAX7219_UpdateDisplaysReg(MAX7219_Frame(MAX7219_SCAN_LIMIT_REG, 0x07)); /* Set scan limit to 8 digits */
+    MAX7219_UpdateDisplaysReg(MAX7219_Frame(MAX7219_DISPLAY_TEST_REG, 0x00)); /* Disable test mode */
 
     return OP_SUCCESS;
 }
 
-MAX7219_ReturnCodes MAX7219_UpdateDisplayReg(size_t dispOffset, uint16_t frame) {
-    if (dispOffset > MAX_DISP_OFFSET(conf.numOfDisplays)) { /* If the display offset is greater than the total amount of displays do nothing */
+MAX7219_ReturnCodes MAX7219_UpdateDisplayReg(uint8_t dispOffset, uint16_t frame) {
+    if (dispOffset > MAX_DISP_OFFSET(conf.numOfDisp)) { /* If the display offset is greater than the total amount of displays do nothing */
         return OFFSET_ERROR;
     }
 
-    BSYWait(); /* Make sure that SSP controller is idle and drive pin state low */
+    SSP_BSYWait(); /* Make sure that SSP controller is idle and drive pin state low */
     *(conf.ssel) = LOW;
-    for (size_t i = 1; i <= conf.numOfDisplays; i++) { /* Send frames in reverse order. The first frame will arrive to the last MAX7219 chip */
-        if (dispOffset == (conf.numOfDisplays - i)) {
-            sendSPIFrame(frame); /* Send desired data */
+    for (uint8_t i = 1; i <= conf.numOfDisp; i++) { /* Send frames in reverse order. The first frame will arrive to the last MAX7219 chip */
+        if (dispOffset == (conf.numOfDisp - i)) {
+            SSP_SendFrame(frame); /* Send desired data */
         }
         else {
-            sendSPIFrame(MAX7219_FRAME(MAX7219_NO_OP_REG, 0x00)); /* Send dummy data */
+            SSP_SendFrame(MAX7219_Frame(MAX7219_NO_OP_REG, 0x00)); /* Send dummy data */
         }
     }
-    BSYWait(); /* Wait until the SSP controller is idle to prevent latching data when TX FIFO is not empty and drive pin state high */
+    SSP_BSYWait(); /* Wait until the SSP controller is idle to prevent latching data when TX FIFO is not empty and drive pin state high */
     *(conf.ssel) = HIGH;
 
     return OP_SUCCESS;
 }
 
 void MAX7219_UpdateDisplaysReg(uint16_t frame) {
-    BSYWait(); /* Make sure that SSP controller is idle and drive pin state low */
+    SSP_BSYWait(); /* Make sure that SSP controller is idle and drive pin state low */
     *(conf.ssel) = LOW;
-    for (size_t i = 0; i < conf.numOfDisplays; i++) {
-        sendSPIFrame(frame);
+    for (uint8_t i = 0; i < conf.numOfDisp; i++) {
+        SSP_SendFrame(frame);
     }
-    BSYWait(); /* Wait until the SSP controller is idle to prevent latching data when TX FIFO is not empty and drive pin state high */
+    SSP_BSYWait(); /* Wait until the SSP controller is idle to prevent latching data when TX FIFO is not empty and drive pin state high */
     *(conf.ssel) = HIGH;
 }
 
-MAX7219_ReturnCodes MAX7219_UpdateBuffer(size_t colOffset, const uint8_t *data, size_t bytes, bool singleValue) {
-    /* Parameters validation */
-    if (colOffset > MAX_COL_OFFSET(conf.numOfDisplays) ||
-        bytes == 0 ||
-        NEW_COL_OFFSET(colOffset, bytes) > MAX_COL_OFFSET(conf.numOfDisplays))
-    {
+MAX7219_ReturnCodes MAX7219_UpdateBuffer(uint16_t colOffset, const uint8_t *data, uint16_t bytes, MAX7219_BufferUpdateModes mode) {
+    /* Basic validation. Cast the offset to a 32 bit value to prevent overflow while calculating end column offset */
+    if (bytes == 0 || END_COL_OFFSET((uint32_t) colOffset, bytes) > MAX_COL_OFFSET(conf.numOfDisp)) {
         return OFFSET_ERROR;
     }
 
-    if (singleValue) { /* If single value is passed just copy it N times to the buffer */
-        for (size_t i = colOffset; i <= NEW_COL_OFFSET(colOffset, bytes); i++) {
+    if (mode == VAL_UPDATE) { /* If VAL_UPDATE is passed just copy the value N times to the buffer */
+        for (uint16_t i = colOffset; i <= END_COL_OFFSET(colOffset, bytes); i++) {
             buffer[i] = *data;
         }
-    } else { /* Otherwise copy all the data to the buffer */
-        for (size_t i = colOffset, j = 0; i <= NEW_COL_OFFSET(colOffset, bytes); i++, j++) {
+    } else if (mode == SEQ_UPDATE) { /* Otherwise copy all the data to the buffer */
+        for (uint16_t i = colOffset, j = 0; i <= END_COL_OFFSET(colOffset, bytes); i++, j++) {
             buffer[i] = data[j];
         }
     }
@@ -126,20 +122,20 @@ MAX7219_ReturnCodes MAX7219_UpdateBuffer(size_t colOffset, const uint8_t *data, 
 }
 
 void MAX7219_RefreshDisp(void) {
-    for (size_t i = 0; i < 8; i++) {
-        BSYWait(); /* Make sure that SSP controller is idle and drive pin state low */
+    for (int8_t i = 0; i < COLS_PER_DISP; i++) {
+        SSP_BSYWait(); /* Make sure that the SSP controller is idle and drive pin state low */
         *(conf.ssel) = LOW;
 
-        for (size_t j = 1; j <= conf.numOfDisplays; j++) {
-            sendSPIFrame(MAX7219_FRAME(MAX7219_DIGIT_REG(i), buffer[(conf.numOfDisplays - j) * 8 + i]));
+        for (uint8_t j = 1; j <= conf.numOfDisp; j++) {
+            SSP_SendFrame(MAX7219_Frame(MAX7219_DIG_REG(i), buffer[COL_OFFSET(conf.numOfDisp - j, i)]));
         }
 
-        BSYWait(); /* Wait until the SSP controller is idle to prevent latching data when TX FIFO is not empty and drive pin state high */
+        SSP_BSYWait(); /* Wait until the SSP controller is idle to prevent latching data when TX FIFO is not empty and drive pin state high */
         *(conf.ssel) = HIGH;
     }
 }
 
-void MAX7219_UnsetConfiguration(void) {
+void MAX7219_UnsetConf(void) {
     free(buffer);
     buffer = NULL;
 }
